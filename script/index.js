5e727befeacdb75c84208aec1815646479109a5d
DmitriyIvanovIT <strela199720@gmail.com>
2020-09-23 18:00:57 +0300

    feat: Creating a phone validation | script/index.js script/module/validPhone.js

--- script/index.js
+++ script/index.js
@@ -1,9 +1,11 @@
 import openPhone from './module/openPhone.js';
 import toggleMenu from './module/toggleMenu.js';
 import togglePopupRepair from './module/togglePopupRepair.js';
+import validPhone from './module/validPhone.js';
 
 
 openPhone();
 toggleMenu();
 window.addEventListener('resize', toggleMenu);
 togglePopupRepair();
+validPhone('#feedback-input1');
